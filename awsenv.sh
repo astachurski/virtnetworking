dcde6ce994db878733223cb0def62eda1d551231
astachurski <astachurski@gmail.com>
2016-02-01 22:00:19 +0100

    add new files

---
+++ awsenv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID=AKIAJ6TRLEECGCVAQCYA
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_REGION=eu-central-1
+export BOTO_CONFIG=/Users/adrian/newdev/anstest1/boto.config
